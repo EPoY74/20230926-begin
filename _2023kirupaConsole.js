0e0597a0dad6cd7e9f2f06d099c6a3476fd3c1e3
EPoY74 <127658418+EPoY74@users.noreply.github.com>
2023-11-11 20:13:01 +0500

    \Вывожу на пряямую в лог, разбил на 2 файла

---
+++ _2023kirupaConsole.js
@@ -0,0 +1,7 @@
+let myButton = document.querySelector("#myButton");
+myButton.addEventListener("click", doSomething, false)
+
+function doSomething(e){
+    //alert ("Is this working?")
+    console.log("Is this working&" );
+}
