14e0b2b32804194ff0a967117dc50a46e42bbdb0
EPoY74 <127658418+EPoY74@users.noreply.github.com>
2023-11-11 20:27:39 +0500

    Изучаю консоль и  различные выводы в нее

--- _2023kirupaConsole.js
+++ _2023kirupaConsole.js
@@ -1,8 +1,23 @@
 let myButton = document.querySelector("#myButton");
+let counter = 0
+
 myButton.addEventListener("click", doSomething, false)
 
+
 function doSomething(e){
+    counter++;
+
     //alert ("Is this working?")
     console.log("Is this working&" );
     console.log("We clicked on: " + e.target.id)
+    console.log("Button clicked " + counter + "times!")
+
+    if (counter == 3){
+        showMore()
+    }
+}
+
+function showMore(){
+    console.warn("This is warning!")
+    console.error("This is error!")
 }
