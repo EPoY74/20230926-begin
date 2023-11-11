c6761360d142c3c4375b84cceea9bf66bf9a2372
EPoY74 <127658418+EPoY74@users.noreply.github.com>
2023-11-11 20:16:18 +0500

    Один вопрос только: что за "e"  и откуда взялась

--- _2023kirupaConsole.js
+++ _2023kirupaConsole.js
@@ -4,4 +4,5 @@ myButton.addEventListener("click", doSomething, false)
 function doSomething(e){
     //alert ("Is this working?")
     console.log("Is this working&" );
+    console.log("We clicked on: " + e.target.id)
 }
