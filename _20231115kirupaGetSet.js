758143d4bdf357053cf172bd7ceb96b824246b65
EPoY74 <127658418+EPoY74@users.noreply.github.com>
2023-11-15 12:28:00 +0500

    Изучаю Геттеры и Сеттеры

---
+++ _20231115kirupaGetSet.js
@@ -0,0 +1,33 @@
+let zorb = {
+    message: "Hello Kitty!!!",
+
+    get greeting() {
+        return this.message;
+    },
+
+    set greeting(value) {
+        this.message = value;
+    } 
+}
+
+let shout = {
+    _message: "HELLO!",
+
+    get message(){
+        return this._message;
+    },
+
+    set message(value){
+        this._message = value.toUpperCase();
+    }
+};
+
+console.log(zorb.greeting)
+zorb.greeting = "Omg!!"
+console.log(zorb.greeting)
+
+
+console.log(shout.message)
+shout.message = "Hello Kitty!"
+console.log(shout.message)
+
