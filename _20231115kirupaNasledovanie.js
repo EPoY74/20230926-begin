934c976a712a67eba92f0503c15cb0f6ef981949
EPoY74 <127658418+EPoY74@users.noreply.github.com>
2023-11-15 21:05:02 +0500

    Не работает цикл - не возврящается длинна массива

--- _20231115kirupaNasledovanie.js
+++ _20231115kirupaNasledovanie.js
@@ -10,7 +10,23 @@ let person = {
             return "The initials is " + this.firstName[0] + this.lastName[0]
         } 
     }
-};
+}
+
+
+function shuffle(inputarr) {
+    console.log("<------------------------------------->")
+    console.log(inputarr)
+    for (let i = inputarr.length - 1; i >= 0; i--) {
+
+        let randomIndex = Math.floor(Math.random() * (i + 1))
+        console.log(randomIndex + " " + i)
+        let itemAtIndex = inputarr[randomIndex]
+
+        inputarr[randomIndex] = inputarr[i]
+        inputarr[i] = itemAtIndex
+    }
+    return inputarr
+}
 
 let funnyGay = Object.create(person);
 funnyGay.firstName = "Conan"
@@ -27,3 +43,13 @@ detective.lastName = "Monk"
 console.log(detective.getName())
 console.log(detective.getInitials())
 // console.log(detective.prototype()) - Попробовал посмотреть прототип - не получилось
+
+
+let shuffleArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(shuffleArray.length)
+
+console.log(shuffleArray);
+
+shuffle(shuffleArray);
+
+console.log(shuffleArray);
