86bea109bf3ec7fcc13ac82e892e4ecc10a0231d
EPoY74 <127658418+EPoY74@users.noreply.github.com>
2023-11-17 19:45:25 +0500

    Изчаю QuerySelector 1

--- _20231116kirupaQuerySelectorLearn.js
+++ _20231116kirupaQuerySelectorLearn.js
@@ -1,5 +1,8 @@
-let mainDivElement = document.querySelector("#main")
-let imagesElements = document.querySelectorAll(".theImage")
+let mainDivElement = document.querySelector("#main")// если целимся на  конкратный элемент с id = "main"
+let imagesElements = document.querySelectorAll(".theImage") // возвращает все найденные элементы
+let allImages = document.querySelectorAll("img") // ЕНсли надо нацелиться на все изображения
+let mehImage = document.querySelectorAll("img[src='meh.png']")//целюсь на изображение, чей атрибут src = meh.pngzz
+
 
 for (let i = 0; i < imagesElements.length; i++){
     let image = imagesElements[i]
